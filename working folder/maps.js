564f27da12d050c14b68e1094b31a107c02856f2
aliciay <alicia.yoosin@gmail.com>
2018-03-03 23:25:14 -0500

    some map circles and markers

--- working folder/maps.js
+++ working folder/maps.js
@@ -1,3 +1,28 @@
+Array.prototype.repeat= function(what, L){
+ while(L) this[--L]= what;
+ return this;
+}
+
+var rad = function(x) {
+  return x * Math.PI / 180;
+};
+
+var getDistance = function(p1, p2) {
+  var R = 6378137; // Earth’s mean radius in meter
+  var dLat = rad(p2[0] - p1[0]);
+  var dLong = rad(p2[1] - p1[1]);
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1[0])) * Math.cos(rad(p2[0])) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d; // returns the distance in meter
+};
+
+var pos0= [0,0];
+var beachMarker;
+var mapMarkerPos = [].repeat([0,0],10);
+
 function initializeMap() {
   console.log('initialize');
 
@@ -8,10 +33,11 @@ function initializeMap() {
   });
   map.setTilt(45);
 
-  var image = 'https://i.imgur.com/WQFbLCW.png';
-  image.height = 1;
-  image.width = 2;
-  var beachMarker = new google.maps.Marker({
+  var image = {
+    url : 'https://i.imgur.com/WQFbLCW.png',
+    scaledSize: new google.maps.Size(50, 50),
+  };
+   beachMarker = new google.maps.Marker({
           map: map,
           icon: image,
           draggable: true,
@@ -25,13 +51,29 @@ function initializeMap() {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
+            pos0[0]=pos.lat;
+            pos0[1]=pos.lng;
             beachMarker.setPosition(pos);
             map.setCenter(pos);
+            var shape = {
+         coords: [1, 1, 1, 20, 18, 20, 18, 1],
+         type: 'poly'
+       };
 
-            var textbox = 'https://i.imgur.com/6OVfGMf.png';
-            textbox.height = 2;
-            textbox.width = 1;
+
+       var circle = new google.maps.Circle({
+         map: map,
+         radius: 100,    // 10 miles in metres
+         fillColor: '#dddddd',
+         strokeColor: '#ffd21e',
+            strokeOpacity: 0.5,
+            strokeWeight: 2,
+            fillOpacity: 0.35,
+       });
+       circle.bindTo('center', beachMarker, 'position');
+
+            var textbox = 'https://i.imgur.com/oFukjry.png';
+
             for (var i = 0; i < 10; i++) {
                  var ran = Math.random() * 0.005 - 0.002;
                  var ran2 = Math.random() * 0.005 - 0.002;
@@ -42,13 +84,25 @@ function initializeMap() {
                   var marker = new google.maps.Marker({
                     map: map,
                     icon: textbox,
+                    shape: shape,
                     title: "new story",
-                    zIndex: i
+                    zIndex: i,
+                    opacity: 0.8,
+                    //replace this url to next page
+                    url: 'https://i.imgur.com/RpaZ6oa.png',
                   }
                 )
                 marker.setPosition(pos1);
-              }
+                mapMarkerPos[i][0]=pos1.lat;
+                mapMarkerPos[i][1]=pos1.lng;
+                google.maps.event.addListener(marker, 'click', function() {
 
+                  var dist =getDistance(pos0, mapMarkerPos[marker.zIndex])
+                  if(dist < 100)
+                    { console.log(dist);
+                      window.location.href = marker.url;
+                    }});
+              }
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
@@ -56,7 +110,5 @@ function initializeMap() {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
     }
-
-
 
 }
